bd13d67b17f0f3b1bf0e1bf0d07fc9913b356cfb
GulnaraYarmuhametova <yarmuhametova.gulnara@yandex.ru>
2018-04-01 22:26:09 +0500

    second

---
+++ Positions.java
@@ -0,0 +1,32 @@
+package dataPackage;
+
+public enum Positions {
+    NONE("Не выбрано"),
+    LECTURER("Преподаватель"),
+    SENIOR_LECTURER("Старший преподователь"),
+    DOCENT("Доцент"),
+    PROFESSOR("Профессор");
+
+    private final String text;
+    private static final String[] array;
+
+    static {
+        array = new String[Positions.values().length];
+        for(Positions value : Positions.values()) {
+            array[value.ordinal()] = value.toString();
+        }
+    }
+
+    Positions(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public static String[] toArray() {
+        return array;
+    }
+}
