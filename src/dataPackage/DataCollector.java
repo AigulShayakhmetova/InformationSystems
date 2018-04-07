182d2045e9cc22bbf96d327470bbdc8859bb48b3
GulnaraYarmuhametova <37900886+GulnaraYarmuhametova@users.noreply.github.com>
2018-04-07 20:12:58 +0500

    Update DataCollector.java

--- src/dataPackage/DataCollector.java
+++ src/dataPackage/DataCollector.java
@@ -9,20 +9,24 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class DataCollector implements Serializable {
-    public ArrayList<Lecturer> lecturers;
-    public ArrayList<Student> freeStudents;
+public class DataCollector implements Serializable /** сериализация объектов класса (создание клона) */
+{
+    public ArrayList<Lecturer> lecturers;/** массив преподавателей */
+    public ArrayList<Student> freeStudents; /** массив свободны студентов */ 
     private String dataCollectorFileName = "dataCollector";
 
-    public DataCollector() {
+    public DataCollector() /** конструктор */
+    {
         lecturers = new ArrayList<Lecturer>();
         freeStudents = new ArrayList<Student>();
-        File file = new File(dataCollectorFileName);
-        if(file.exists() && !file.isDirectory()) {
-            DataCollector dataCollector = deSerializeFromFile(file);
-            if(dataCollector != null) {
-                lecturers.addAll(dataCollector.lecturers);
-                freeStudents.addAll(dataCollector.freeStudents);
+        File file = new File(dataCollectorFileName);/** определние файла */
+        if(file.exists() && !file.isDirectory()) /** файл существует и он не переполнен */
+        {
+            DataCollector dataCollector = deSerializeFromFile(file); /** десериализация для файла (восстановление)  */
+            if(dataCollector != null) /** файл не пустой */ 
+            {
+                lecturers.addAll(dataCollector.lecturers); /** добавляем (восстанавливаем )  преподавателя  */
+                freeStudents.addAll(dataCollector.freeStudents); /** добавляем (востанавливаем ) студента  */
             }
         }
         System.out.println("DataCollector::DataCollector(); -- lecturers.size():" + lecturers.size());
@@ -31,30 +35,37 @@ public class DataCollector implements Serializable {
         }
     }
 
-    public void addLecturer(Lecturer newLecturer) {
+    public void addLecturer(Lecturer newLecturer) /** добавление преподвателя */
+    {
         lecturers.add(newLecturer);
         serializeToFile();
     }
 
-    public Lecturer addStudent(Student newStudent) {
-        for (Lecturer lecturer : lecturers) {
-            if (lecturer.students.size() < lecturer.maxStudens) {
-                lecturer.students.add(newStudent);
-                serializeToFile(); // twice
+    public Lecturer addStudent(Student newStudent) /** добавление студента преподавателю */
+    {
+        
+        for (Lecturer lecturer : lecturers) 
+        {
+            if (lecturer.students.size() < lecturer.maxStudens) /** преподаватель имеет возможность взять студента */
+            {
+                lecturer.students.add(newStudent); /** добавляем к преподавателю еще одного студента */
+                serializeToFile(); 
                 return lecturer;
             }
         }
-        freeStudents.add(newStudent);
-        serializeToFile(); // twice
+        freeStudents.add(newStudent);/** добавление свободного студента */
+        serializeToFile(); 
         return null;
     }
 
-    public void serializeToFile() {
+    public void serializeToFile() /** сериализация файла с объектами  */
+    {
         System.out.println("DataCollector::serializeToFile(); -- Try!");
-        try {
+        try
+        {
             FileOutputStream fos = new FileOutputStream(dataCollectorFileName);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(this);
+            oos.writeObject(this); /** добавление в файл */
             oos.close();
             fos.close();
             System.out.println("DataCollector::serializeToFile(); -- Good!");
@@ -63,36 +74,37 @@ public class DataCollector implements Serializable {
         }
     }
 
-    public static DataCollector deSerializeFromFile(File file) {
+    public static DataCollector deSerializeFromFile(File file)/**  десериализация файла с объектами */ 
+    {
         System.out.println("DataCollector::deSerializeFromFile(); -- Try! file.getName():" + file.getName());
-        try {
+        try 
+        {
             FileInputStream fis = new FileInputStream(file.getName());
             ObjectInputStream ois = new ObjectInputStream(fis);
-            DataCollector dataCollector = (DataCollector) ois.readObject();
+            DataCollector dataCollector = (DataCollector) ois.readObject(); /** чтение файла с объектами */
             ois.close();
             fis.close();
-            System.out.println("DataCollector::deSerializeFromFile(); -- Good!");
+            System.out.println("DataCollector::deSerializeFromFile(); -- Good!"); /** десериализация прошла успешно */
             for(Lecturer lecturer : dataCollector.lecturers) {
-                System.out.println("DataCollector::deSerializeFromFile(); -- lecturer:" + lecturer);
+                System.out.println("DataCollector::deSerializeFromFile(); -- lecturer:" + lecturer); /** вывод обавленного ранее преподавателя с параметрами */
             }
             return dataCollector;
         } catch (IOException ioe) {
             System.err.println("DataCollector::deSerializeFromFile(); -- ioe:" + ioe);
             file.deleteOnExit();
-//            ioe.printStackTrace();
-            return null;
+            return null; /** что - то пошло не так */
         } catch (ClassNotFoundException c) {
             System.err.println("DataCollector::deSerializeFromFile(); -- Class not found | c:" + c);
-//            c.printStackTrace();
             return null;
         }
     }
     
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("DataCollector:[");
-        stringBuilder.append("lecturers.size():" + lecturers.size());
-        stringBuilder.append(", " + "freeStudents.size():" + freeStudents.size());
+    public String toString() 
+    {
+        StringBuilder stringBuilder = new StringBuilder(); /** создание строчки */
+        stringBuilder.append("DataCollector:["); 
+        stringBuilder.append("lecturers.size():" + lecturers.size()); /** количество преподавателей */
+        stringBuilder.append(", " + "freeStudents.size():" + freeStudents.size()); /* количество студентов */
         stringBuilder.append("]");
         return stringBuilder.toString();
     }
