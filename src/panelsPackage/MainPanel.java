ffafd9e93cafbda1e7d5bcedd372723b6a66f3bc
GulnaraYarmuhametova <37900886+GulnaraYarmuhametova@users.noreply.github.com>
2018-04-07 17:49:38 +0500

    Update MainPanel.java

--- src/panelsPackage/MainPanel.java
+++ src/panelsPackage/MainPanel.java
@@ -8,30 +8,35 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class MainPanel extends JPanel {
-    public MainPanel(PanelsManager panelsManager) {
-        System.out.println("MainPanel::MainPanel(); -- this.getClass().getName():" + this.getClass().getName());
+    public MainPanel(PanelsManager panelsManager) /** конструктор */
+    {
+        System.out.println("MainPanel::MainPanel(); -- this.getClass().getName():" + this.getClass().getName());/**вывод объекта через его имя*/
         setName(this.getClass().getName());
 
-        JButton lectureButton = new JButton("Преподаватель");
-        lectureButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                panelsManager.setLecturePanel();
+        JButton lectureButton = new JButton("Преподаватель");/** создание кнопки "Прнеподаватель" */
+        lectureButton.addActionListener(new ActionListener()   /** добавление прподавателя*/
+                                        {
+            public void actionPerformed(ActionEvent arg0)  
+            {
+                panelsManager.setLecturePanel();/** установка панели "Преподаватель" */
             }
         });
-        JButton studentButton = new JButton("Студент");
-        studentButton.addActionListener(new ActionListener() {
+        JButton studentButton = new JButton("Студент");/** создание кнопки "Студент" */
+        studentButton.addActionListener(new ActionListener() /** добавление студента */
+                                        {
             public void actionPerformed(ActionEvent arg0) {
-                panelsManager.setStudentPanel();
+                panelsManager.setStudentPanel();/** установка панели "Стидент" */
             }
         });
         setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
         add(lectureButton);
         add(studentButton);
 
-        JButton btnInfoList = new JButton("Список");
-        btnInfoList.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                panelsManager.setInfoListPanel();
+        JButton btnInfoList = new JButton("Список");/** создание кнопки "Список" */
+        btnInfoList.addActionListener(new ActionListener()/** добавление преподавателя */
+                                      {
+            public void actionPerformed(ActionEvent arg0) { 
+                panelsManager.setInfoListPanel(); /** установка панели "Список" */
             }
         });
         add(btnInfoList);
