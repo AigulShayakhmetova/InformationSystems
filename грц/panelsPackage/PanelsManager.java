47eedd6ca9a853e0baa9e615864ce58beb620098
AigulShayakhmetova <37963394+AigulShayakhmetova@users.noreply.github.com>
2018-04-08 16:42:41 +0500

    Update PanelsManager.java

--- грц/panelsPackage/PanelsManager.java
+++ грц/panelsPackage/PanelsManager.java
@@ -28,42 +28,45 @@ public class PanelsManager/** публичный класс */
         setMainPanel();/**установка панели*/
     }
 
-    public void setMainPanel() /**установка панели*/
+    public void setMainPanel() /**установка самой главной панели*/
     {
         if(mainPanel == null) /**если панели не было */
         {
             mainPanel = new MainPanel(this);/**устанавливаем панель */ 
         }
-        container.add(mainPanel.getName(), mainPanel);/**заносим панель в контейнер */
+        container.add(mainPanel.getName(), mainPanel);/**заносим главную панель в контейнер */
         cardLayout.show(container, mainPanel.getName());/** отображаем панель */
         mainAppletClass.setSize(container.getPreferredSize());
     }
 
-    public void setLecturePanel() {
+    public void setLecturePanel() /**установка панели преподавателя*/
+    {
         if(lecturerPanel == null) {
             lecturerPanel = new LecturerPanel(this, dataCollector);
         }
-        container.add(lecturerPanel.getName(), lecturerPanel);
-        cardLayout.show(container, lecturerPanel.getName());
+        container.add(lecturerPanel.getName(), lecturerPanel);/**заносим панель преподавателя в контейнер */
+        cardLayout.show(container, lecturerPanel.getName());/** отображаем панель */
         mainAppletClass.setSize(container.getPreferredSize());
     }
 
-    public void setStudentPanel() {
+    public void setStudentPanel()/**установка панели студента*/
+    {
         if(studentPanel == null) {
             studentPanel = new StudentPanel(this, dataCollector);
         }
-        container.add(studentPanel.getName(), studentPanel);
-        cardLayout.show(container, studentPanel.getName());
+        container.add(studentPanel.getName(), studentPanel);/**заносим панель студента в контейнер */
+        cardLayout.show(container, studentPanel.getName());/** отображаем панель */
         mainAppletClass.setSize(container.getPreferredSize());
     }
 
-    public void setInfoListPanel() {
+    public void setInfoListPanel() /**установка панели со списком преподавателей и студентов*/
+    {
         if(infoListPanel == null) {
             infoListPanel = new InfoListPanel(this, dataCollector);
         }
         infoListPanel.updateInfoList();
-        container.add(infoListPanel.getName(), infoListPanel);
-        cardLayout.show(container, infoListPanel.getName());
+        container.add(infoListPanel.getName(), infoListPanel);/**заносим данную панель в контейнер */
+        cardLayout.show(container, infoListPanel.getName());/** отображаем панель */
         mainAppletClass.setSize(container.getPreferredSize());
     }
 
